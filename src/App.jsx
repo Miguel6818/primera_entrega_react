556f699299d7f9e4a836e6c1bf43aa784ffd68ee
Miguel6818 <angelmiguel6818717@gmail.com>
2024-10-03 18:02:49 -0500

    publicar pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
 
-import Carrito from './components/carrito'
+
 import ItemListContainer from './components/ItemListContainer'
 import NavBar from './components/NavBar'
-import Badge from './components/Badge'
+
 
 function App() {  
   return (
